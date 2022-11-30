805dfac4d591cf3fa738386645ece789683008e8
LuisGerardo1999 <luislopez.202005348@gmail.com>
2022-11-30 10:17:38 -0600

    cambios animales

--- Assets/Scripts/NWLobo.cs
+++ Assets/Scripts/NWLobo.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class NWLobo : MonoBehaviour
-{
-    // Start is called before the first frame update
+public class NWLobo : Mensajes
+{   
+    [SerializeField] private GameObject pointer;
+    public NavMeshAgent EnemyNaveMesh;
+    public Transform TransformPointer;
+    Animator animator;
+    int isRunningHash;
+    int isCloseHash;
+    public Transform TransformPlayer;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        isRunningHash = Animator.StringToHash("IsRunning");
+        isCloseHash = Animator.StringToHash("IsClose");  
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        bool isClose = animator.GetBool(isCloseHash);
+        bool isRunning = animator.GetBool(isRunningHash);
+        float dist = Vector3.Distance(transform.position, TransformPointer.position);
+
+        if(dist <= 20)
+        {
+            EnemyNaveMesh.destination = pointer.transform.position;
+            animator.SetBool(isRunningHash, true);
+
+        }
+        if(dist <= 5) 
+        {
+            EnemyNaveMesh.destination = EnemyNaveMesh.transform.position;
+            animator.SetBool(isCloseHash, true);
+
+
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            MensajesUI.text = "El enemigo te ha atrapado";
+            Invoke("ResetearText", 5f);
+        }
     }
 }
